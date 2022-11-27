f0569d373b559dc23ab833a63ed7d7b521a75bf0
Felix Mendez <mendez.felixrn@gmail.com>
2022-11-26 22:46:20 -0500

    create a factory function that creates playes that have name, can attack, and randomly attack

---
+++ src/scripts/player.js
@@ -0,0 +1,27 @@
+import { gameboardFactory } from "./gamebaord";
+
+const playerFactory = (name) => {
+  const gameboard = gameboardFactory();
+
+  const attack = (oppGameboard, coordinate) => {
+    if (oppGameboard.gameboard[coordinate].hasBeenHit == false) {
+      oppGameboard.receiveAttack(coordinate);
+    }
+  };
+
+  const attackRandomly = (oppGameboard) => {
+    let coordinate = createRandomCoordinate();
+    while (oppGameboard.gameboard[coordinate].hasBeenHit == true) {
+      coordinate = createRandomCoordinate();
+    }
+    oppGameboard.receiveAttack(coordinate);
+  };
+
+  const createRandomCoordinate = () => {
+    return Math.floor(Math.random() * 100);
+  };
+
+  return { name, gameboard, attackRandomly, attack };
+};
+
+export { playerFactory };
