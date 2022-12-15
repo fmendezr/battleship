cefb234d1f59682f2946f81d9772609de29029bf
Felix Mendez <mendez.felixrn@gmail.com>
2022-12-14 20:56:44 -0500

    Add axis btn and other fixes

--- src/components/placeShipScreen.js
+++ src/components/placeShipScreen.js
@@ -1,4 +1,4 @@
-const renderPlaceShipScreen = (player) => {
+const renderPlaceShipScreen = (player, shipObj) => {
   const main = document.createElement("main");
   document.body.appendChild(main);
 
@@ -9,8 +9,22 @@ const renderPlaceShipScreen = (player) => {
 
   const subtitle = document.createElement("h2");
   subtitle.classList.add("subtitle");
-  subtitle.textContent = `${player.name} place your ship:`;
+  subtitle.textContent = `${player.name} place your ${shipObj.type}:`;
   main.appendChild(subtitle);
+
+  const axisBtn = document.createElement("button");
+  axisBtn.textContent = "Axis: X";
+  axisBtn.value = "x";
+  main.appendChild(axisBtn);
+  axisBtn.addEventListener("click", () => {
+    if (axisBtn.value == "x") {
+      axisBtn.value = "y";
+      axisBtn.textContent = "Axis: Y";
+    } else {
+      axisBtn.value = "x";
+      axisBtn.textContent = "Axis: X";
+    }
+  });
 
   const gameboardContainer = document.createElement("div");
   gameboardContainer.classList.add("gameboard");
