36b230a29ae30413b523f4663fa85f019ec36690
Felix Mendez <mendez.felixrn@gmail.com>
2022-12-16 18:48:59 -0500

    basic components  (no interactivity)

---
+++ src/components/combatScreen.js
@@ -0,0 +1,41 @@
+const renderCombatScreen = (player, opp) => {
+  const main = document.createElement("main");
+  main.id = "main";
+  document.body.appendChild(main);
+
+  const title = document.createElement("h1");
+  title.classList.add("smallTitle");
+  title.textContent = "Battleship";
+  main.appendChild(title);
+
+  const gameboardsContainer = document.createElement("div");
+  gameboardsContainer.classList.add("gameboardsContainer");
+  main.appendChild(gameboardsContainer);
+
+  const playerGameBoard = document.createElement("div");
+  playerGameBoard.classList.add("gameboard");
+  gameboardsContainer.appendChild(playerGameBoard);
+
+  for (let i = 0; i < player.gameboard.gameboard.length; i++) {
+    let cell = document.createElement("div");
+    cell.id = i;
+    cell.classList.add("cell");
+    playerGameBoard.appendChild(cell);
+    if (player.gameboard.gameboard[i].containsShip != false) {
+      cell.classList.add("containsShip");
+    }
+  }
+
+  const oppGameboard = document.createElement("div");
+  oppGameboard.classList.add("gameboard");
+  gameboardsContainer.appendChild(oppGameboard);
+
+  for (let i = 0; i < player.gameboard.gameboard.length; i++) {
+    let cell = document.createElement("div");
+    cell.id = i;
+    cell.classList.add("cell");
+    oppGameboard.appendChild(cell);
+  }
+};
+
+export { renderCombatScreen };
